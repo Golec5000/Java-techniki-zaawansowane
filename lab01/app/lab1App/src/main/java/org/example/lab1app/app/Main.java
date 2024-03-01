aef5ac90a2329e64cff67d628099874d7b4d1118
Golec5000 <danielRyszkowski@gmail.com>
2024-03-01 23:24:02 +0100

    description added and making jlik by maven

--- lab01/app/lab1App/src/main/java/org/example/lab1app/app/Main.java
+++ lab01/app/lab1App/src/main/java/org/example/lab1app/app/Main.java
@@ -1,5 +1,22 @@
 package org.example.lab1app.app;
 
+/**
+ * @Author: Daniel Ryszkowski aka BWP
+ * @Project: lab1App
+ * @Date: 2020-10-07
+ * @Version: 1.0
+ *
+ * @Description:
+ * For clean project structure in console run: mvn clean
+ * For build project in console run: mvn compile
+ * For run project in console run: mvn javafx:run
+ * For link project in console run: mvn javafx:jlink
+ *
+ * After creating zip file with jlink, unzip it
+ * go to bin folder and in console run:
+ * java -m org.example.lab1app/org.example.lab1app.app.Main
+ *
+ */
 public class Main {
     public static void main(String[] args) {
         Lab01Application.main(args);
